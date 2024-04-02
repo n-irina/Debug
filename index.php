6f0ab1440fb2767f7049876c7469941e2ec4699b
Irina NAHOANIKO <irina.nahoaniko@gmail.com>
2024-04-02 20:00:57 +0200

    debug demo

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+   $characters = [
+      "Negan" => [
+         "city" => "The Sanctuary",
+         "weapon" => "Lucille"
+       ],
+      "Daryl" => [
+         "city" => "Alexandria",
+         "weapon" => "crossbow"
+       ],
+      "Ezekiel" => [
+         "city" => "The Kingdom",
+         "weapon" => "Shiva"
+       ],
+      "Nahoaniko" => [
+         "city" => "Melun Zoo",
+         "weapon" => "magic keyboard"
+       ]
+   ];
+var_dump($characters);
+?>
